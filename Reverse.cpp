7fcc6d592f098daa557f73d28e94f48948a7ee11
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-03-30 02:27:16 +0000

    further recursion attempts...

--- Reverse.cpp
+++ Reverse.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include <sstream>
+
 #include "Reverse.h"
 
 // Reverse class constructor.
@@ -12,8 +14,30 @@ Reverse::Reverse()
 // Takes in a integer and reverses it's order.
 int Reverse::reverseDigit( int value )
 {
-	
+	// Setting the int value to a string variable.
+	string fwdStr = static_cast<ostringstream*>( &(ostringstream() << value) )->str();
+	string revStr;
+	int i = fwdStr.length()-1;
 
+//	for ( int i = fwdStr.length()-1; i > 0 ;i-- )
+
+	cout << "fwdStr in loop: " << fwdStr << endl;
+	revStr += fwdStr[i];
+	cout << "revStr in loop: " << revStr << endl;
+
+	// Setting the string revStr to a integer variable.
+	int rev;
+	if ( ! (istringstream(revStr) >> rev) ) rev = 0;
+	cout << "revStr: " << revStr << endl;
+	i--;
+
+	if ( revStr.length() == fwdStr.length() )
+	{
+		return rev;
+	}
+	else{
+		reverseDigit( value );
+	}
 }
 
 // Takes in a string and reverses it's order.

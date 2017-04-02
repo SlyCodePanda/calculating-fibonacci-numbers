2130afbf53388e8ea9c793d074be2d12a6668741
a1703500 <a1703500@ccf68bb9-d9f7-4bd8-892d-a681e0b0e2f6>
2017-04-02 08:08:56 +0000

    full function

---
+++ 2017/s1/adds/assignment4/EfficientFibonacci.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+#include "EfficientFibonacci.h"
+
+// Efficient Fibonacci class constructor.
+EfficientFibonacci::EfficientFibonacci()
+{
+
+}
+
+// Takes in an integer and returns it's Fibonacci number.
+int EfficientFibonacci::effFib( int n )
+{
+	if( ( n == 1 )||( n == 2 ) )
+	{
+		return 1;
+	}
+	else
+	{
+		return effFib( n-2 )+effFib( n-1 );
+	}
+}
+
+// Effiecient Fibonacci class destructor.
+EfficientFibonacci::~EfficientFibonacci()
+{
+
+}
--- 2017/s1/adds/assignment4/Fibonacci.cpp
+++ 2017/s1/adds/assignment4/Fibonacci.cpp
@@ -12,16 +12,14 @@ Fibonacci::Fibonacci()
 // Takes in an integer and returns it's Fibonacci number.
 int Fibonacci::fib( int n )
 {
-	if ( n == 0 )
-	{
-		return 0;
-	}
-	if ( n == 1 )
+	if( ( n == 1 )||( n == 2 ) )
 	{
 		return 1;
 	}
-
-	return fib( n-1 )+fib( n-2 );
+	else
+	{
+		return fib( n-2 )+fib( n-1 );
+	}
 }
 
 // Fibonacci class destructor.
